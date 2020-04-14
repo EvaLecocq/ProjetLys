21a9606b5858efc37578700b6aacb4d174c9f133
EvaLecocq <evalecocq@free.fr>
2020-04-14 21:00:02 +0200

    7e

---
+++ Assets/script/UI/UI_Herbier.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UI_Herbier : MonoBehaviour
+{
+    
+    public GameObject carte;
+    public GameObject inventaire;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OpenHerbier()
+    {
+        carte.gameObject.SetActive(true);
+        inventaire.gameObject.SetActive(true);
+       
+    }
+    public void CloseHerbier()
+    {
+        carte.gameObject.SetActive(false);
+        inventaire.gameObject.SetActive(false);
+    }
+}
