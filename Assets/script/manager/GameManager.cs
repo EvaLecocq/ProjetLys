9e6872b52fcdc42d3472cb54914d577a98379b43
crazyk1k1 <crazyk1k1@hotmail.com>
2020-04-16 13:03:03 +0100

    systeme loot, toutes plantes et obj ajouter + modif gameManager + 2 props lampadaire interactif

--- Assets/script/manager/GameManager.cs
+++ Assets/script/manager/GameManager.cs
@@ -24,6 +24,32 @@ public class GameManager : MonoBehaviour
     public int day;
     private DayNightCycle cycle;
 
+    [Header("fleur")]
+    public int Tulipe;
+    public int CirseCommun;
+    public int Chrysantheme;
+    public int Orchidee;
+    public int Hibiscus;
+    public int PaeoniaOfficinalis;
+    public int EuphorbeReveilleMatin;
+    public int GazaniaRigens;
+    public int HelleboreOrient;
+    public int FumariaOfficinalis;
+    public int BleuMarie;
+    public int AncolieDuCanada;
+    public int Kalanchoe;
+    public int Gerbera;
+    public int AngeliqueDesEstuaires;
+    public int Agapanthe;
+    public int Rose;
+    public int Fritillaire;
+    public int FleurDeLys;
+
+    [Header("objet")]
+    public int pierre;
+    public int baton;
+    public int gland;
+
     // Start is called before the first frame update
     void Start()
     {
