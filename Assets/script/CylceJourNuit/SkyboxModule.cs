f190bcf7ed645b859d2a2561836c63ba818ac2a3
crazyk1k1 <crazyk1k1@hotmail.com>
2020-04-21 11:46:36 +0100

    meteo debut + nuages fini

--- Assets/script/CylceJourNuit/SkyboxModule.cs
+++ Assets/script/CylceJourNuit/SkyboxModule.cs
@@ -4,14 +4,56 @@ using UnityEngine;
 
 public class SkyboxModule : DNModuleBase {
 
+
+    public enum temp { soleil, pluie, tempete, brouillard}
+    public temp meteo;
+
+    [Header("soleil")]
     [SerializeField]
     private Gradient skyColor;
     [SerializeField]
     private Gradient horizonColor;
 
+    [Header("pluie")]
+    [SerializeField]
+    private Gradient skyColorPluie;
+    [SerializeField]
+    private Gradient horizonColorPluie;
+
+    [Header("tempete")]
+    [SerializeField]
+    private Gradient skyColorTempete;
+    [SerializeField]
+    private Gradient horizonColorTempete;
+
+    [Header("brouillard")]
+    [SerializeField]
+    private Gradient skyColorBrouillard;
+    [SerializeField]
+    private Gradient horizonColorBrouillard;
+
     public override void UpdateModule(float intensity)
     {
-        RenderSettings.skybox.SetColor("_SkyTint", skyColor.Evaluate(intensity));
-        RenderSettings.skybox.SetColor("_GroundColor", horizonColor.Evaluate(intensity));
+        if(meteo == SkyboxModule.temp.soleil)
+        {
+            RenderSettings.skybox.SetColor("_SkyTint", skyColor.Evaluate(intensity));
+            RenderSettings.skybox.SetColor("_GroundColor", horizonColor.Evaluate(intensity));
+        }
+        if (meteo == SkyboxModule.temp.pluie)
+        {
+            RenderSettings.skybox.SetColor("_SkyTint", skyColorPluie.Evaluate(intensity));
+            RenderSettings.skybox.SetColor("_GroundColor", horizonColorPluie.Evaluate(intensity));
+        }
+        if (meteo == SkyboxModule.temp.brouillard)
+        {
+            RenderSettings.skybox.SetColor("_SkyTint", skyColorBrouillard.Evaluate(intensity));
+            RenderSettings.skybox.SetColor("_GroundColor", horizonColorBrouillard.Evaluate(intensity));
+        }
+        if (meteo == SkyboxModule.temp.tempete)
+        {
+            RenderSettings.skybox.SetColor("_SkyTint", skyColorTempete.Evaluate(intensity));
+            RenderSettings.skybox.SetColor("_GroundColor", horizonColorTempete.Evaluate(intensity));
+        }
+
     }
 }
--- Assets/script/environement/cloudManager.cs
+++ Assets/script/environement/cloudManager.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class cloudManager : MonoBehaviour
 {
+    public enum mode { soleil, pluie, tempete, brouillard }
+    public mode meteoActive;
+
+    private SkyboxModule skyColor;
+
     public bool useRootPoint = true;
     public GameObject rootPoint;
 
     public GameObject[] cloudRound;
     public float[] cloudRoundSpeed;
 
+    public GameObject pluie;
+    public GameObject tempete;
+    public GameObject brouillard;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        skyColor = SkyboxModule.FindObjectOfType<SkyboxModule>();
     }
 
     // Update is called once per frame
@@ -27,6 +36,39 @@ public class cloudManager : MonoBehaviour
         for (int i = 0; i < cloudRound.Length; i++)
         {
             cloudRound[i].transform.Rotate(Vector3.up * cloudRoundSpeed[i]);
+        }
+
+        if (meteoActive == cloudManager.mode.soleil)
+        {
+            pluie.SetActive(false);
+            brouillard.SetActive(false);
+            tempete.SetActive(false);
+
+            skyColor.meteo = SkyboxModule.temp.soleil;
+        }
+        if (meteoActive == cloudManager.mode.pluie)
+        {
+            pluie.SetActive(true);
+            brouillard.SetActive(false);
+            tempete.SetActive(false);
+
+            skyColor.meteo = SkyboxModule.temp.pluie;
+        }
+        if (meteoActive == cloudManager.mode.tempete)
+        {
+            pluie.SetActive(false);
+            brouillard.SetActive(false);
+            tempete.SetActive(true);
+
+            skyColor.meteo = SkyboxModule.temp.tempete;
+        }
+        if (meteoActive == cloudManager.mode.brouillard)
+        {
+            pluie.SetActive(false);
+            brouillard.SetActive(true);
+            tempete.SetActive(false);
+
+            skyColor.meteo = SkyboxModule.temp.brouillard;
         }
     }
 }
