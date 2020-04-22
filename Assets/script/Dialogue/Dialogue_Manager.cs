2c12dae436c2576e6daf1e58e5829dc0b66c1c28
ClementProject <cousseauclement@gmail.com>
2020-04-22 09:42:34 +0200

    Image inventaire+ UI et système de dialogue de base

---
+++ Assets/script/Dialogue/Dialogue_Manager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue_Manager : MonoBehaviour
+{
+    public Text Textname;
+    public Text dialogue;
+    private Queue<string> sentences;
+    public GameObject dialogueapparition;
+    public GameObject quete;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+   
+    public void StartDialogue(Dialogue dialogue)
+    {
+        dialogueapparition.gameObject.SetActive(true);
+        Debug.Log("Sart" + dialogue.name);
+
+        Textname.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+    public void StartDialogue2(Dialogue2 _dialogue2)
+    {
+        dialogueapparition.gameObject.SetActive(true);
+        Debug.Log("Sart" + _dialogue2.name);
+
+        Textname.text = _dialogue2.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in _dialogue2.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence2();
+    }
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+    public void DisplayNextSentence2()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue2();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogue.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogue.text += letter;
+            yield return null;
+        }
+    }
+    public void EndDialogue()
+    {
+        dialogueapparition.gameObject.SetActive(false);
+
+            //quete.gameObject.SetActive(true);
+        
+    }
+    public void EndDialogue2()
+    {
+        dialogueapparition.gameObject.SetActive(false);
+
+       
+
+    }
+
+}
---
+++ Assets/script/Dialogue/Dialogue_Trigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue_Trigger : MonoBehaviour
+{
+    public Dialogue _dialogue;
+    public Dialogue2 _dialogue2;
+    
+    
+ 
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") 
+        {
+           
+               
+              //  FindObjectOfType<Dialogue_Manager>().StartDialogue2(_dialogue2);
+          
+                FindObjectOfType<Dialogue_Manager>().StartDialogue(_dialogue);
+           
+                
+            
+            
+        }
+    }
+  
+}
