4b0c3eec8818c24091250742d17fbfaf29f89b15
ClementProject <cousseauclement@gmail.com>
2020-04-23 09:46:28 +0200

    UIcarte

--- Assets/script/environement/cloudManager.cs
+++ Assets/script/environement/cloudManager.cs
@@ -4,71 +4,33 @@ using UnityEngine;
 
 public class cloudManager : MonoBehaviour
 {
-   
+    public enum mode { soleil, pluie, tempete, brouillard }
+    public mode meteoActive;
 
-    [Header("eclairage")]
     private SkyboxModule skyColor;
-    private DayNightCycle sunColor;
-
-    private float sunIntensityBase;
-    private float sunIntensityVariationBase;
-
-    public float sunIntensityPluie;
-    public float sunIntensityVariationPluie;
-
-    public float sunIntensityTempete;
-    public float sunIntensityVariationTempete;
-
-    [Header("nuage")]
-    public Material[] nuageColor;
-    private List<Transform> tableauNuage;
 
     public bool useRootPoint = true;
     public GameObject rootPoint;
 
-    public Transform[] cloudRound;
+    public GameObject[] cloudRound;
     public float[] cloudRoundSpeed;
     public float[] cloudRoundSpeedDefault;
     public float[] cloudSpeedMultiplicatorTempete;
 
-    public int morningCloudAppear;
-    public int nightCloudDispawn;
-
-    [Header("meteo")]
     public GameObject pluie;
     public GameObject tempete;
     public GameObject brouillard;
-
-    public GameObject[] thunder;
-    public float speedSpawn;
-    
 
     // Start is called before the first frame update
     void Start()
     {
         skyColor = SkyboxModule.FindObjectOfType<SkyboxModule>();
-        sunColor = DayNightCycle.FindObjectOfType<DayNightCycle>();
 
-        sunIntensityBase = sunColor.sunBaseIntensity;
-        sunIntensityVariationBase = sunColor.sunVariation;
 
         for (int i = 0; i < cloudRoundSpeedDefault.Length; i++)
         {
             cloudRoundSpeedDefault[i] = cloudRoundSpeed[i];
         }
-
-        tableauNuage = new List<Transform>();
-
-        for (int i = 0; i < cloudRound.Length; i++)
-        {
-            foreach (Transform go in cloudRound[i])
-            {
-                tableauNuage.Add(go.transform);
-            }
-        }
-
-        InvokeRepeating("ThunderStruck", 0f, speedSpawn);
-
     }
 
     // Update is called once per frame
@@ -81,22 +43,10 @@ public class cloudManager : MonoBehaviour
 
         for (int i = 0; i < cloudRound.Length; i++)
         {
-            cloudRound[i].Rotate(Vector3.up * cloudRoundSpeed[i]);
+            cloudRound[i].transform.Rotate(Vector3.up * cloudRoundSpeed[i]);
         }
 
-        meteoChange();
-
-        if(GameManager.s_Singleton.meteoActive != GameManager.mode.tempete)
-        {
-            CloudSpawn();
-        }
-       
-    }
-
-    public void meteoChange()
-    {
-        
-        if (GameManager.s_Singleton.meteoActive == GameManager.mode.soleil)
+        if (meteoActive == cloudManager.mode.soleil)
         {
             pluie.SetActive(false);
             brouillard.SetActive(false);
@@ -108,17 +58,8 @@ public class cloudManager : MonoBehaviour
             {
                 cloudRoundSpeed[i] = cloudRoundSpeedDefault[i];
             }
-
-            foreach(Transform tr in tableauNuage)
-            {
-                tr.gameObject.GetComponent<Renderer>().material = nuageColor[0];
-            }
-
-            sunColor.sunBaseIntensity = sunIntensityBase;
-            sunColor.sunVariation = sunIntensityVariationBase;
         }
-
-        if (GameManager.s_Singleton.meteoActive == GameManager.mode.pluie)
+        if (meteoActive == cloudManager.mode.pluie)
         {
             pluie.SetActive(true);
             brouillard.SetActive(false);
@@ -130,17 +71,8 @@ public class cloudManager : MonoBehaviour
             {
                 cloudRoundSpeed[i] = cloudRoundSpeedDefault[i];
             }
-
-            foreach (Transform tr in tableauNuage)
-            {
-                tr.gameObject.GetComponent<Renderer>().material = nuageColor[1];
-            }
-
-            sunColor.sunBaseIntensity = sunIntensityPluie;
-            sunColor.sunVariation = sunIntensityVariationPluie;
         }
-
-        if (GameManager.s_Singleton.meteoActive == GameManager.mode.tempete)
+        if (meteoActive == cloudManager.mode.tempete)
         {
             pluie.SetActive(false);
             brouillard.SetActive(false);
@@ -152,19 +84,8 @@ public class cloudManager : MonoBehaviour
             {
                 cloudRoundSpeed[i] = cloudSpeedMultiplicatorTempete[i];
             }
-
-            foreach (Transform tr in tableauNuage)
-            {
-                tr.gameObject.GetComponent<Renderer>().material = nuageColor[2];
-            }
-
-            sunColor.sunBaseIntensity = sunIntensityTempete;
-            sunColor.sunVariation = sunIntensityVariationTempete;
-
-            
         }
-
-        if (GameManager.s_Singleton.meteoActive == GameManager.mode.brouillard)
+        if (meteoActive == cloudManager.mode.brouillard)
         {
             pluie.SetActive(false);
             brouillard.SetActive(true);
@@ -176,48 +97,6 @@ public class cloudManager : MonoBehaviour
             {
                 cloudRoundSpeed[i] = cloudRoundSpeedDefault[i];
             }
-
-            foreach (Transform tr in tableauNuage)
-            {
-                tr.gameObject.GetComponent<Renderer>().material = nuageColor[1];
-            }
-
-            sunColor.sunBaseIntensity = sunIntensityPluie;
-            sunColor.sunVariation = sunIntensityVariationPluie;
         }
-    }
-
-    public void ThunderStruck()
-    {
-        foreach (GameObject go in thunder)
-        {
-            go.SetActive(false);
-        }
-
-        int range = Random.Range(0, thunder.Length);
-
-        thunder[range].SetActive(true);
-
-        
-    }
-
-    public void CloudSpawn()
-    {
-        if (GameManager.s_Singleton.time >= nightCloudDispawn || GameManager.s_Singleton.time < morningCloudAppear)
-        {
-            foreach(Transform go in cloudRound)
-            {
-                go.gameObject.SetActive(false);
-            }
-
-        }
-        else
-        {
-            foreach (Transform go in cloudRound)
-            {
-                go.gameObject.SetActive(true);
-            }
-        }
-
     }
 }
