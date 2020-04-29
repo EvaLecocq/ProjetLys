53905fd88ea985810e8c7fbbb13464ee5f9545ed
ClementProject <cousseauclement@gmail.com>
2020-04-29 10:33:07 +0200

    UI_modif control

--- Assets/script/UI/UI_Herbier.cs
+++ Assets/script/UI/UI_Herbier.cs
@@ -10,6 +10,9 @@ public class UI_Herbier : MonoBehaviour
     public GameObject inventaire;
     public GameObject carte;
     public GameObject buttoncarte;
+    public bool openhebier = false;
+    public bool openInv = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,29 +22,63 @@ public class UI_Herbier : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if(openhebier == false)
         {
-            herbier.gameObject.SetActive(true);
-            buttoncarte.SetActive(false);
-           
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                herbier.gameObject.SetActive(true);
+                buttoncarte.SetActive(false);
+                openhebier = true;
+             
+
+            }
         }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                if(openhebier == true)
+                {
+                    herbier.gameObject.SetActive(false);
+                    openhebier = false;
+                }
+               
+            }
+        }
+        if(openInv == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                inventaire.gameObject.SetActive(true);
+                openInv = true;
+            }
+          
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                if(openInv == true)
+                {
+                    inventaire.gameObject.SetActive(false);
+                    openInv = false;
+                }
+            }
+        }
+      
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            herbier.gameObject.SetActive(false);
-            inventaire.gameObject.SetActive(false);
-            buttoncarte.gameObject.SetActive(true);
+            herbier.gameObject.SetActive(false);            
+           // buttoncarte.gameObject.SetActive(true);
             carte.gameObject.SetActive(false);
 
         }
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            inventaire.gameObject.SetActive(true);
-        }
-        if (Input.GetKeyDown(KeyCode.M))
+       
+      /*  if (Input.GetKeyDown(KeyCode.M))
         {
             carte.gameObject.SetActive(true);
             buttoncarte.gameObject.SetActive(false);
-        }
+        }*/
     }
     public void OpenHerbier()
     {
