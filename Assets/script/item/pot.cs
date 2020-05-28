7e7690a4b22f6e4c5c8cb87bd211708f38d1123c
crazyk1k1 <crazyk1k1@hotmail.com>
2020-05-28 14:34:10 +0100

    rewardPot

--- Assets/script/item/pot.cs
+++ Assets/script/item/pot.cs
@@ -15,87 +15,96 @@ public class pot : MonoBehaviour
     public Outline outlinerItem;
     public GameObject fleurDePot;
     public GameObject interactionIcon;
+    public GameObject particuleReward;
+    public Transform rewardSpot;
+
+    public void reward()
+    {
+        fleurDePot.SetActive(true);
+
+        Instantiate(particuleReward, rewardSpot.position, rewardSpot.rotation);
+    }
 
     public void PlaceFlower()
     {
         if (fleurType == pot.fleur.Agapanthe && GameManager.s_Singleton.Agapanthe > 0)
         {
-            fleurDePot.SetActive(true);
+            reward();
         }
         else if (fleurType == pot.fleur.AncolieDuCanada && GameManager.s_Singleton.AncolieDuCanada > 0)
         {
-            fleurDePot.SetActive(true);
+            reward();
         }
         else if (fleurType == pot.fleur.AngeliqueDesEstuaires && GameManager.s_Singleton.AngeliqueDesEstuaires > 0)
         {
-            fleurDePot.SetActive(true);
+            reward();
         }
        
         else if (fleurType == pot.fleur.BleuMarie && GameManager.s_Singleton.BleuMarie > 0)
         {
-            fleurDePot.SetActive(true);
+            reward();
         }
         else if (fleurType == pot.fleur.Chrysantheme && GameManager.s_Singleton.Chrysantheme > 0)
         {
-            fleurDePot.SetActive(true);
+            reward();
         }
         else if (fleurType == pot.fleur.CirseCommun && GameManager.s_Singleton.CirseCommun > 0)
         {
-            fleurDePot.SetActive(true);
+            reward();
         }
         else if (fleurType == pot.fleur.EuphorbeReveilleMatin && GameManager.s_Singleton.EuphorbeReveilleMatin > 0)
         {
-            fleurDePot.SetActive(true);
+            reward();
         }
         else if (fleurType == pot.fleur.FleurDeLys && GameManager.s_Singleton.FleurDeLys > 0)
         {
-            fleurDePot.SetActive(true);
+            reward();
         }
         else if (fleurType == pot.fleur.Fritillaire && GameManager.s_Singleton.Fritillaire > 0)
         {
-            fleurDePot.SetActive(true);
+            reward();
         }
         else if (fleurType == pot.fleur.FumariaOfficinalis && GameManager.s_Singleton.FumariaOfficinalis > 0)
         {
-            fleurDePot.SetActive(true);
+            reward();
         }
         else if (fleurType == pot.fleur.GazaniaRigens && GameManager.s_Singleton.GazaniaRigens > 0)
         {
-            fleurDePot.SetActive(true);
+            reward();
         }
         else if (fleurType == pot.fleur.Gerbera && GameManager.s_Singleton.Gerbera > 0)
         {
-            fleurDePot.SetActive(true);
+            reward();
         }
        
         else if (fleurType == pot.fleur.HelleboreOrient && GameManager.s_Singleton.HelleboreOrient > 0)
         {
-            fleurDePot.SetActive(true);
+            reward();
         }
         else if (fleurType == pot.fleur.Hibiscus && GameManager.s_Singleton.Hibiscus > 0)
         {
-            fleurDePot.SetActive(true);
+            reward();
         }
         else if (fleurType == pot.fleur.Kalanchoe && GameManager.s_Singleton.Kalanchoe > 0)
         {
-            fleurDePot.SetActive(true);
+            reward();
         }
         else if (fleurType == pot.fleur.Orchidee && GameManager.s_Singleton.Orchidee > 0)
         {
-            fleurDePot.SetActive(true);
+            reward();
         }
         else if (fleurType == pot.fleur.PaeoniaOfficinalis && GameManager.s_Singleton.PaeoniaOfficinalis > 0)
         {
-            fleurDePot.SetActive(true);
+            reward();
         }
 
         else if (fleurType == pot.fleur.Rose && GameManager.s_Singleton.Rose > 0)
         {
-            fleurDePot.SetActive(true);
+            reward();
         }
         else if (fleurType == pot.fleur.Tulipe && GameManager.s_Singleton.Tulipe > 0)
         {
-            fleurDePot.SetActive(true);
+            reward();
         }
 
 
