0585bb5285b3bb2b0b59cb4ea02b9ead502f5a2b
crazyk1k1 <crazyk1k1@hotmail.com>
2020-06-10 09:43:48 +0100

    chemin du travelling avec nouv texte + item gland + agent P + lapin modif

--- Assets/script/manager/cameraTravelling.cs
+++ Assets/script/manager/cameraTravelling.cs
@@ -15,6 +15,7 @@ public class cameraTravelling : MonoBehaviour
     void Start()
     {
         manager = Dialogue_Manager.FindObjectOfType<Dialogue_Manager>();
+       
     }
 
     // Update is called once per frame
@@ -23,7 +24,7 @@ public class cameraTravelling : MonoBehaviour
        // Debug.Log(manager.sentences.Count);
        if(GameManager.s_Singleton.progression == 0)
         {
-
+            //Debug.Log(manager.sentences.Count);  13
             parcourer = manager.sentences.Count;
 
             if (Input.GetKeyDown(KeyCode.E))
@@ -36,29 +37,34 @@ public class cameraTravelling : MonoBehaviour
                         cm.Priority = 0;
                     }
 
-                if(parcourer == 5)
+
+                if (parcourer <= 13 && parcourer >= 11)
+                {
+                    cam[0].Priority = 25;
+                }
+                if (parcourer == 10)
                 {
                     cam[1].Priority = 25;
                 }
-                if (parcourer == 4)
+                if (parcourer == 9 || parcourer == 8)
                 {
                     cam[2].Priority = 25;
                 }
-                if (parcourer == 3)
+                if (parcourer <= 7 && parcourer >= 3)
                 {
-                    cam[3].Priority = 25;
+                    cam[0].Priority = 25;
                 }
                 if (parcourer == 2)
                 {
-                    cam[4].Priority = 25;
+                    cam[3].Priority = 25;
                 }
                 if (parcourer == 1)
                 {
-                    cam[5].Priority = 25;
+                    cam[4].Priority = 25;
                 }
                 if (parcourer == 0)
                 {
-                    cam[6].Priority = 25;
+                    cam[5].Priority = 25;
                 }
 
 
